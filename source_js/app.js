9fed8e078a1a1190e4e50572384d43aee9aa14b4
mm135 <lucasung51@gmail.com>
2016-03-09 21:27:22 -0600

    done

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,26 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('mp3',['ngRoute', 'imdbControllers']);
 
 app.config(function ($routeProvider) {
+		
+		$routeProvider
+		//list route
+        .when('/index', {
+            templateUrl : './partials/list.html',
+            controller  : 'listController',
+            controllerAs: 'lc'
+        })
+		//gallery route
+        .when('/gallery', {
+            templateUrl : './partials/gallery.html',
+            controller  : 'galleryController',
+        })
+        //details route
+        .when('/details/:rank', {
+            templateUrl : './partials/details.html',
+            controller  : 'detailsController',
+        })
 
+        .otherwise({
+        	redirectTo: '/index'
+        });
 })
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,53 @@
-/* Sample Controller */
+var imdbControllers = angular.module('imdbControllers', []);
+
+//controller for the list view and sorting
+imdbControllers.controller('listController', ['$scope', '$http', function($scope, $http) {
+
+		$scope.query = {};
+		$scope.query.term = '';
+		$scope.query.sortBy = 'rank';
+		$scope.query.reverse = '';
+
+	$http.get('./data/imdb250.json').success(function(data) {
+		$scope.movieData = data;
+	});
+
+}]);
+
+//controller for the gallery view
+imdbControllers.controller('galleryController', ['$scope', '$http', function($scope, $http) {
+
+	// var genres = {};
+
+	$http.get('./data/imdb250.json').success(function(data) {
+
+		$scope.movieData = data;
+		
+		// angular.forEach($scope.movieData, function(movie) {
+		// 	for(var i=0; i<movie.genre.length; i++) {
+		// 		genres[movie.genre[i]] = 1;
+		// 	}
+		// });
+
+		// $scope.genres = Object.keys(genres);
+
+	});
+
+}]);
+
+//details controller
+imdbControllers.controller('detailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+		
+	$scope.currMovie;
+
+	$http.get('./data/imdb250.json').success(function(data) {
+		$scope.movieData = data;
+
+		angular.forEach($scope.movieData, function(item) {
+			if($routeParams.rank == item.rank) {
+				$scope.currMovie = item;
+				return;
+			}
+		});
+	});
+}]);
